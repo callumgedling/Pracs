e40ccd40d4c02302244fa2e152631cc994cc649e
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-08-22 12:17:15 +1000

    Add all prac files to git

---
+++ Prac_02/answers.py
@@ -0,0 +1,5 @@
+# on line one i saw the number 8. The smalles number i could have seen was 5, the largest was 20.
+
+# I saw the number 3. The the smalles number i could have seen was a 3, the largest was a 9. Line 2 could not produce a 4
+
+# i saw 3.2951693193317793. smallest number = 2.5, largest = 5.5.
