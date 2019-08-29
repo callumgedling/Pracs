f1d601b3f587b31f50d8bb60551c1f450440900a
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-08-29 12:55:27 +1000

    Fix total_income

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,16 +13,16 @@ def main():
         income = float(input("Enter income for month {} :".format(month)))
         incomes.append(income)
 
-    calculate_income(incomes, number_of_months)
+    calculate_income(incomes)
 
 
-def calculate_income(incomes, number_of_months):
+def calculate_income(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month, income in enumerate(incomes):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month + 1, income, total))
 
 
 main()
