033b486a6ef56411412a8ba6df797bc8a71b0c6b
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-09-11 12:46:34 +1000

    Improve formatting

--- Prac_06/languages.py
+++ Prac_06/languages.py
@@ -4,10 +4,9 @@ ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
+
 def main():
     print(python)
-
-
 
 
 main()
