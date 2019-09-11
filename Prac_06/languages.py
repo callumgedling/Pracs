1d0c780a3440bc04532bc0d6172caa6748195134
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-09-11 12:46:12 +1000

    Complete intermediate excercise

---
+++ Prac_06/languages.py
@@ -0,0 +1,13 @@
+from Prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+def main():
+    print(python)
+
+
+
+
+main()
