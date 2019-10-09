785146951bfa3ddc64b04910e6b3f2839adc6ece
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-10-09 11:57:53 +1000

    Complete dynamic widgets part of prac

---
+++ Prac_07/dynamic_widgets_two.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+
+class DynamicWidgetsAppTwo(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Callum", "Frank", "Andrea", "Keegan"]
+
+    def build(self):
+        self.title = "Dynamic Widgets Two"
+        self.root = Builder.load_file('dynamic_widgets_two.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.names:
+            temp_button = Button(text=name, id=name)
+            self.root.ids.names_box.add_widget(temp_button)
+            temp_button.bind(on_release=self.press_entry)
+
+    def press_entry(self, instance):
+        name = instance.id
+        self.status_text = "{}".format(name)
+
+
+DynamicWidgetsAppTwo().run()
